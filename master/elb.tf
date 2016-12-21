952014da23befb31ab6e976bd1c5dc6969df473f
Ringo De Smet <ringo@de-smet.name>
2016-12-21 14:05:19 +0100

    Bump the loadbalancers module.

--- master/elb.tf
+++ master/elb.tf
@@ -1,5 +1,5 @@
 module "master_elb" {
-  source            = "github.com/skyscrapers/terraform-loadbalancers//elb_no_ssl_no_s3logs?ref=d1be8a322a1fa5e757ffa6c011aec66b000d10cd"
+  source            = "github.com/skyscrapers/terraform-loadbalancers//elb_no_ssl_no_s3logs?ref=be1e9c0f5b030a8a46609954b701a0882551e93d"
   name              = "k8s-master"
   subnets           = ["${var.subnets}"]
   project           = "${var.project}"
@@ -11,10 +11,20 @@ module "master_elb" {
   health_target     = "HTTP:8080/healthz"
   internal          = true
   backend_sg        = ["${aws_security_group.masters.id}"]
+  backend_sg_count  = 1
 }
 
 resource "aws_elb_attachment" "master-attach" {
   elb      = "${module.master_elb.elb_id}"
   count    = "${var.amount_masters}"
   instance = "${element(module.masters.instance_ids, count.index)}"
+}
+
+resource "aws_security_group_rule" "allow_backend_healthcheck" {
+  security_group_id = "${module.master_elb.sg_id}"
+  type = "egress"
+  from_port = 8080
+  to_port = 8080
+  protocol = "TCP"
+  source_security_group_id = "${aws_security_group.masters.id}"
 }
