5e087bb2bd9baa2a9f33db75eec97419bcbfde59
Ringo De Smet <ringo@de-smet.name>
2017-02-24 10:10:34 +0100

    Generating the subnets dynamically.

--- cluster/main.tf
+++ cluster/main.tf
@@ -2,6 +2,66 @@ terraform {
 	required_version = "> 0.8.0"
 }
 
+# TODO list:
+#  * Allow configuring the API ELB type: Public or Internal
+#  * Use separate management and node subnets once the fix for this is released:
+#      https://github.com/kubernetes/kops/issues/1980
+#
+
+data "aws_vpc" "vpc_for_k8s" {
+  id = "${var.vpc_id}"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+### NOTE: Do not change the layout of the template files or your will get unnecessary empty lines
+###       in the generated output.
+
+# Private subnets to host the Kubernetes master nodes
+data template_file "master-subnet-spec" {
+  count = "3"
+  template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
+
+  vars {
+    name = "management-${count.index}"
+    type = "Private"
+    zone = "${element(data.aws_availability_zones.available.names, count.index)}"
+    id   = ""
+    cidr = "${cidrsubnet(data.aws_vpc.vpc_for_k8s.cidr_block,8,var.master_net_number+count.index)}"
+  }
+}
+
+# Private subnets to host the Kubernetes worker nodes
+# Currently disabled due to https://github.com/kubernetes/kops/issues/1980
+# data template_file "node-subnet-spec" {
+#   count = "3"
+#   template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
+
+#   vars {
+#     name = "node-${count.index}"
+#     type = "Private"
+#     zone = "${element(data.aws_availability_zones.available.names, count.index)}"
+#     id   = ""
+#     cidr = "${cidrsubnet(data.aws_vpc.vpc_for_k8s.cidr_block,8,var.node_net_number+count.index)}"
+#   }
+# }
+
+# Utility subnets are public for ELB creation.
+data template_file "utility-subnet-spec" {
+  count = "3"
+  template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
+
+  vars {
+    name = "utility-${count.index}"
+    type = "Utility"
+    zone = "${element(data.aws_availability_zones.available.names, count.index)}"
+    id   = ""
+    cidr = "${cidrsubnet(data.aws_vpc.vpc_for_k8s.cidr_block,8,var.utility_net_number+count.index)}"
+  }
+}
+
 data template_file "cluster-spec" {
   template = "${file("${path.module}/../templates/kops-cluster.tpl.yaml")}"
 
@@ -9,6 +69,13 @@ data template_file "cluster-spec" {
     project = "${var.project}"
     environment = "${var.environment}"
     name = "${var.name}"
+    k8s_version = "${var.k8s_version}"
+    vpc_id = "${data.aws_vpc.vpc_for_k8s.id}"
+    vpc_cidr = "${data.aws_vpc.vpc_for_k8s.cidr_block}"
+    k8s_data_bucket = "${var.k8s_data_bucket}"
+    master_subnets = "${join("\n",data.template_file.master-subnet-spec.*.rendered)}"
+    #node_subnets = "${join("\n",data.template_file.node-subnet-spec.*.rendered)}"
+    utility_subnets = "${join("\n",data.template_file.utility-subnet-spec.*.rendered)}"
   }
 }
 
--- cluster/variables.tf
+++ cluster/variables.tf
@@ -12,7 +12,9 @@ variable "key_name" {}
 #   default = "t2.medium"
 # }
 
-# variable "k8s_data_bucket" {}
+variable "k8s_data_bucket" {
+	description = "S3 bucket to store the kops cluster description & state"
+}
 
 # variable "master_instance_type" {
 #   default = "t2.medium"
@@ -24,20 +26,33 @@ variable "key_name" {}
 #   type = "list"
 # }
 
-# variable "master_subnets" {
-#   type = "list"
+variable "master_net_number" {
+	description = "The network number to start with for master subnet cidr calculation"
+}
+
+# Currently disabled due to https://github.com/kubernetes/kops/issues/1980
+# variable "node_net_number" {
+# 	description = "The network number to start with for node subnet cidr calculation"
 # }
+
+variable "utility_net_number" {
+	description = "The network number to start with for utility subnet cidr calculation"
+}
 
 variable "project" {}
 
 variable "environment" {}
 
+variable "vpc_id" {
+	description = "Deploy the Kubernetes cluster in this VPC"
+}
+
 # variable "max_amount_workers" {}
 
 # variable "amount_masters" {}
 
-# variable "cluster_cidr" {}
-
-# variable "k8s_version" {}
+variable "k8s_version" {
+	description = "Kubernetes Version to deploy"
+}
 
 # variable "service_ip_range" {}
