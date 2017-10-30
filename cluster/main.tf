f19846b02fcc110c70cd98beaedede1390dd15ef
Ringo De Smet <ringo@de-smet.name>
2017-10-30 13:05:18 +0100

    Integrate the etcd2 backup configuration into the kops rollout (#15)
    * Adding the systemd files to activate etcd2 backup.
    
    * Simplifying the setup + cleaning up after each backup run.
    
    * Prevent double interpolation of the master & worker instance group templates.
    
    * Additional escaping to make sure the output renders "${DOCKER_CONTAINER}"
    Source: https://github.com/hashicorp/terraform/issues/1942#issuecomment-251298610
    
    * Also escaping the backticks so the command is not interpreted at "terraform apply" time but remains as a string in the output.
    
    * Adding a missing dash just after the equal sign.
    
    * Fix calico kubelet bug for kubernetes 1.6
    
    In newer kops version the calico file gets written as *.conflist. Kubernetes 1.6 can't work with this file. This moves the file from .conflist to .conf.
    
    * Add path file for the path
    
    * Move calico fix to cluster level
    
    * Remove our fixes for calico again as we updated to k8s 1.7.
    Also needed to create an extra service to first create the file with the correct container id before invoking the backup service.

--- cluster/main.tf
+++ cluster/main.tf
@@ -15,11 +15,11 @@ data "aws_availability_zones" "available" {
 
 data "aws_ami" "kubernetes_ami" {
   most_recent = true
+
   # k8s-1.6-debian-jessie-amd64-hvm-ebs-2017-05-02
   name_regex = "^k8s-${element(split(".",var.k8s_version),0)}.${element(split(".",var.k8s_version),1)}-debian-jessie-amd64-hvm-ebs.*"
-  owners = ["383156758163"]
+  owners     = ["383156758163"]
 }
-
 
 #########################################################
 # Subnets
@@ -27,7 +27,7 @@ data "aws_ami" "kubernetes_ami" {
 
 # Private subnets to host the Kubernetes master nodes
 data template_file "master-subnet-spec" {
-  count = "3"
+  count    = "3"
   template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
 
   vars {
@@ -41,7 +41,7 @@ data template_file "master-subnet-spec" {
 
 # Private subnets to host the Kubernetes worker nodes
 data template_file "worker-subnet-spec" {
-  count = "3"
+  count    = "3"
   template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
 
   vars {
@@ -55,7 +55,7 @@ data template_file "worker-subnet-spec" {
 
 # Utility subnets are public for ELB creation.
 data template_file "utility-subnet-spec" {
-  count = "3"
+  count    = "3"
   template = "${file("${path.module}/../templates/kops-cluster-subnet.tpl.yaml")}"
 
   vars {
@@ -72,15 +72,16 @@ data template_file "utility-subnet-spec" {
 #########################################################
 
 data template_file "master-instancegroup-spec" {
-  count = 3
+  count    = 3
   template = "${file("${path.module}/../templates/kops-instancegroup-master.tpl.yaml")}"
 
   vars {
-    name           = "${element(formatlist("master-%s", data.aws_availability_zones.available.names),count.index)}"
-    cluster-name   = "${var.name}"
-    kubernetes_ami = "${data.aws_ami.kubernetes_ami.name}"
-    subnets        = "${element(formatlist("  - master-%s", data.aws_availability_zones.available.names),count.index)}"
-    instance_type  = "${var.master_instance_type}"
+    name            = "${element(formatlist("master-%s", data.aws_availability_zones.available.names),count.index)}"
+    cluster-name    = "${var.name}"
+    k8s_data_bucket = "${var.k8s_data_bucket}"
+    kubernetes_ami  = "${data.aws_ami.kubernetes_ami.name}"
+    subnets         = "${element(formatlist("  - master-%s", data.aws_availability_zones.available.names),count.index)}"
+    instance_type   = "${var.master_instance_type}"
   }
 }
 
@@ -88,15 +89,14 @@ data template_file "worker-instancegroup-spec" {
   template = "${file("${path.module}/../templates/kops-instancegroup-worker.tpl.yaml")}"
 
   vars {
-    name          = "workers"
-    cluster-name  = "${var.name}"
+    name           = "workers"
+    cluster-name   = "${var.name}"
     kubernetes_ami = "${data.aws_ami.kubernetes_ami.name}"
-    subnets       = "${join("\n", formatlist("  - worker-%s", data.aws_availability_zones.available.names))}"
-    instance_type = "${var.worker_instance_type}"
-    min           = "${length(data.aws_availability_zones.available.names)}"
-    max           = "${var.max_amount_workers}"
+    subnets        = "${join("\n", formatlist("  - worker-%s", data.aws_availability_zones.available.names))}"
+    instance_type  = "${var.worker_instance_type}"
+    min            = "${length(data.aws_availability_zones.available.names)}"
+    max            = "${var.max_amount_workers}"
   }
-
 }
 
 #########################################################
@@ -105,7 +105,7 @@ data template_file "worker-instancegroup-spec" {
 
 data template_file "cluster-etcd-member-spec" {
   template = "${file("${path.module}/../templates/kops-cluster-etcd-member.tpl.yaml")}"
-  count = "3"
+  count    = "3"
 
   vars {
     name    = "${element(formatlist("master-%s", data.aws_availability_zones.available.names),count.index)}"
@@ -117,32 +117,34 @@ data template_file "cluster-spec" {
   template = "${file("${path.module}/../templates/kops-cluster.tpl.yaml")}"
 
   vars {
-    name                  = "${var.name}"
-    k8s_version           = "${var.k8s_version}"
-    vpc_id                = "${data.aws_vpc.vpc_for_k8s.id}"
-    vpc_cidr              = "${data.aws_vpc.vpc_for_k8s.cidr_block}"
-    elb_type              = "${var.elb_type}"
-    k8s_data_bucket       = "${var.k8s_data_bucket}"
-    oidc_issuer_url       = "${var.oidc_issuer_url}"
-    etcd_members_main     = "${join("\n",data.template_file.cluster-etcd-member-spec.*.rendered)}"
-    etcd_members_events   = "${join("\n",data.template_file.cluster-etcd-member-spec.*.rendered)}"
-    master_subnets        = "${join("\n",data.template_file.master-subnet-spec.*.rendered)}"
-    worker_subnets        = "${join("\n",data.template_file.worker-subnet-spec.*.rendered)}"
-    utility_subnets       = "${join("\n",data.template_file.utility-subnet-spec.*.rendered)}"
-    master_instance_group = "${join("\n",data.template_file.master-instancegroup-spec.*.rendered)}"
-    worker_instance_group = "${data.template_file.worker-instancegroup-spec.rendered}"
+    name                = "${var.name}"
+    k8s_version         = "${var.k8s_version}"
+    vpc_id              = "${data.aws_vpc.vpc_for_k8s.id}"
+    vpc_cidr            = "${data.aws_vpc.vpc_for_k8s.cidr_block}"
+    elb_type            = "${var.elb_type}"
+    k8s_data_bucket     = "${var.k8s_data_bucket}"
+    oidc_issuer_url     = "${var.oidc_issuer_url}"
+    etcd_members_main   = "${join("\n",data.template_file.cluster-etcd-member-spec.*.rendered)}"
+    etcd_members_events = "${join("\n",data.template_file.cluster-etcd-member-spec.*.rendered)}"
+    master_subnets      = "${join("\n",data.template_file.master-subnet-spec.*.rendered)}"
+    worker_subnets      = "${join("\n",data.template_file.worker-subnet-spec.*.rendered)}"
+    utility_subnets     = "${join("\n",data.template_file.utility-subnet-spec.*.rendered)}"
   }
 }
 
 resource "null_resource" "kops_full_cluster-spec_file" {
   triggers {
-    content = "${data.template_file.cluster-spec.rendered}"
+    content_cluster      = "${data.template_file.cluster-spec.rendered}"
+    content_master_group = "${join("\n",data.template_file.master-instancegroup-spec.*.rendered)}"
+    content_worker_group = "${data.template_file.worker-instancegroup-spec.rendered}"
   }
 
   provisioner "local-exec" {
     command = <<-EOC
       tee ${path.cwd}/kops-cluster.yaml <<EOF
-      ${data.template_file.cluster-spec.rendered}
+      ${join("\n", concat(list(data.template_file.cluster-spec.rendered),
+                          data.template_file.master-instancegroup-spec.*.rendered,
+                          list(data.template_file.worker-instancegroup-spec.rendered)))}
       EOF
       EOC
   }
