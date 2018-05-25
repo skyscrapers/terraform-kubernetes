1bddae774414c9f4f64f5b4db110c48393183869
iuri aranda <iuri.aranda@gmail.com>
2018-05-25 16:37:32 +0200

    Add some basic dashboards to all base deployments
    - k8s-worker-resource-requests-dashboard

---
+++ base/grafana-dashboards.tf
@@ -0,0 +1,3 @@
+data "http" "k8s-worker-resource-requests-dashboard" {
+  url = "https://raw.githubusercontent.com/skyscrapers/grafana-dashboards/master/k8s-workers-resource-requests-dashboard.yaml"
+}
